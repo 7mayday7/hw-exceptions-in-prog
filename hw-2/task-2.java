1422f937cd59c02869fcd8fc3a9a1f164d05ea47
Denis <stroodenis462@mail.ru>
2023-08-27 22:03:15 +0600

    Task 2 hw-2 completed

---
+++ hw-2/task-2.java
@@ -0,0 +1,13 @@
+public class Example {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        try {
+            double d = 0.0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
